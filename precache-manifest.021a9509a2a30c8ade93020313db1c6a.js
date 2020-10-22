6f305f1722b01ea9537570d9609e40472c3e89eb
Deepak3211 <deepakt754@gmail.com>
2020-10-23 00:57:16 +0530

    Updates

---
+++ precache-manifest.021a9509a2a30c8ade93020313db1c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93353e9e030c69d5aa40685796d1674e",
+    "url": "/react-counter-app/index.html"
+  },
+  {
+    "revision": "36fc1bd247ce5108145d",
+    "url": "/react-counter-app/static/css/main.415ab841.chunk.css"
+  },
+  {
+    "revision": "f57640abf0616ae7bd2f",
+    "url": "/react-counter-app/static/js/2.d509916f.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react-counter-app/static/js/2.d509916f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36fc1bd247ce5108145d",
+    "url": "/react-counter-app/static/js/main.d02f4ee5.chunk.js"
+  },
+  {
+    "revision": "8efe71c3078a2d5a0459",
+    "url": "/react-counter-app/static/js/runtime-main.5f43b084.js"
+  }
+]);
